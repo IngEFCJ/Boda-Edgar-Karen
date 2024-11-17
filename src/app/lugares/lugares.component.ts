bed95eb77a6b91dbf383597ebe7ca3b6d3cd67b0
Edgar Cruz <edgar_gnr@hotmail.com>
2024-11-16 22:42:47 -0600

    Se agrega Lugares

--- src/app/lugares/lugares.component.ts
+++ src/app/lugares/lugares.component.ts
@@ -1,3 +1,4 @@
+// lugares.component.ts
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class LugaresComponent {
 
+  // Función para abrir Google Maps en la aplicación del teléfono
+  openGoogleMaps(address: string): void {
+    const googleMapsUrl = `https://www.google.com/maps/search/?q=${encodeURIComponent(address)}`;
+    window.open(googleMapsUrl, '_system'); // Abre la dirección en Google Maps
+  }
 }
