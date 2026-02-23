38a14c9ec76ba06553eaaf41254d342271b5277c
Edgar Francisco Cruz Jauregui <edgar_gnr@hotmail.com>
2026-02-23 02:56:06 -0600

    Se actualizan ligas

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   assetBaseUrl: '/assets/invitaciones',
   assetSas: '', // déjalo vacío si el contenedor es público (Blob anónimo)
-  apiBaseUrl: 'https://localhost:7083/api'
+  apiBaseUrl: 'https://boda-api-e7dyh6hndjg8c4d8.westeurope-01.azurewebsites.net/api'
 };
